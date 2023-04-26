fd00cd539e30a424afd524e68941080131598629
hey-anna <106728222+hey-anna@users.noreply.github.com>
2023-04-27 02:01:21 +0900

    230426_004

--- printfscanf.c
+++ printfscanf.c
@@ -127,12 +127,38 @@ int main(void)
     // str 변수는 총 256개의 공간을 만들었기 때문에, 더 큰 것이 들어오면 문제가 발생할 수 있다.
     // size 256이라고 써서 256개의 문자만 받겠다
 
+    // 프로젝트
+    // 경찰관이 범죄자의 정보를 입수 (조서 작성)
+    // 이름? 나이? 몸무게? 키? 범죄명?
+    char name[256];
+    printf("이름이 뭐예요?");
+    scanf("%s", name);
+
+    int age;
+    printf("몇살이예요?");
+    scanf("%d", &age);
+
+    float weight;
+    printf("몸무게는 몇 kg 이예요?");
+    scanf("%f", &weight);
+
+    double height;
+    printf("키는 몇 cm 이예요?");
+    scanf("%lf", &height);
     
-    // 
-    // 
-    //
-    //
-    //
+    char what[256];
+    printf("무슨 범죄를 저질렀어요?");
+    scanf("%s", what);
+
+    // 조서 내용 출력
+    printf("\n\n--- 범죄자 정보 ---\n\n");
+    printf(" 이름    : %s\n", name);
+    printf(" 나이    : %d\n", age);
+    printf(" 몸무게   : %.2f\n", weight);
+    printf(" 키      : %.2lf\n", height);
+    printf(" 범죄    : %s\n", what);
+
+    // 줄바꿈 \n
     
     return 0;
 }
